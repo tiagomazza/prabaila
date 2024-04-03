2086887adaf2acca045f34c54fb76aa75ba8a522
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-04-03 16:55:29 +0100

    we hope

--- googlesheets.py
+++ googlesheets.py
@@ -1,10 +1,9 @@
 import streamlit as st
-import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 from google.oauth2 import service_account
 import json
 
-# Corrigindo a quebra de linha na chave privada
+# Carregando as credenciais do arquivo JSON
 credentials = json.loads("""
 {
   "type": "service_account",
@@ -15,13 +14,13 @@ credentials = json.loads("""
 """)
 
 # URL da planilha
-
+url = "https://docs.google.com/spreadsheets/d/1j0iFYpsSh3JwQu9ej6g8C9oCfVseQsu2beEPvj512rw/edit#gid=0"
 
 # Conexão com o Google Sheets
 conn = GSheetsConnection("gsheets")
 
 # Leitura dos dados da planilha
-data = conn.read(worksheet="Pag1")
+data = conn.read(url, worksheet="Pag1")
 
 # Filtragem por modelo
 modelos = data['Modelo'].unique()
@@ -35,25 +34,29 @@ numero_filtro = st.sidebar.multiselect('Filtrar por Número', numeros, default=n
 filtro = (data['Modelo'].isin(modelo_filtro)) & (data['Número'].isin(numero_filtro))
 data_filtrada = data[filtro]
 
-# DataFrame para armazenar as alterações de estoque
-estoque_atualizado = data_filtrada.copy()
-
 # Exibição dos dados e atualização do estoque
 for index, row in data_filtrada.iterrows():
     st.write(f"Modelo: {row['Modelo']} - Número: {row['Número']}")
     st.write(f"Descrição: {row['Descrição']}")
     st.write(f"Preço: R${row['Preço']}")
     
-    if index in estoque_atualizado.index:
-        estoque_atualizado.loc[index, 'Estoque'] = st.number_input(f'Estoque atual: {row["Estoque"]}. Quantidade ({row["Modelo"]} - {row["Número"]})', min_value=-10, max_value=10, step=1, value=0)
+    estoque_atualizado = st.number_input(f'Estoque atual: {row["Estoque"]}. Quantidade ({row["Modelo"]} - {row["Número"]})', min_value=-10, max_value=10, step=1, value=row['Estoque'])
+    data_filtrada.loc[index, 'Estoque'] = estoque_atualizado
 
 # Botão para atualizar o estoque
 if st.button("Atualizar Estoque"):
     gc = service_account.Credentials.from_service_account_info(credentials)
     spreadsheet = gc.open_by_url(url)
-    worksheet = spreadsheet.get_worksheet(0)
+    worksheet = spreadsheet.worksheet("Pag1")
 
-    for i, (index, row) in enumerate(estoque_atualizado.iterrows()):
-        worksheet.update_cell(index + 2, 6, row['Estoque'])
+    # Atualizar estoque em lotes de 100 linhas
+    batch_size = 100
+    num_rows = data_filtrada.shape[0]
+
+    for i in range(0, num_rows, batch_size):
+        batch_data = data_filtrada.iloc[i:i+batch_size]
+        cell_range = f"F{i+2}:F{i+2+len(batch_data)-1}"
+        batch_values = [[value] for value in batch_data['Estoque'].tolist()]
+        worksheet.update(cell_range, batch_values)
 
     st.success('Estoque atualizado com sucesso!')
