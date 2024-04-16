2e12b687f2ddb7d72be8f194da7f584ac3b9d61d
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-04-16 17:19:05 +0100

    formulario

--- codeisfun.py
+++ codeisfun.py
@@ -1,38 +1,12 @@
 import streamlit as st
-import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 
 st.title("Google Sheets as a DataBase")
 
-# Function to create a sample Orders dataframe
-def create_orders_dataframe():
-    return pd.DataFrame({
-        'OrderID': [101, 102, 103, 104, 105],
-        'CustomerName': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
-        'ProductList': ['ProductA, ProductB', 'ProductC', 'ProductA, ProductC', 'ProductB, ProductD', 'ProductD'],
-        'TotalPrice': [200, 150, 250, 300, 100],
-        'OrderDate': ['2023-08-18', '2023-08-19', '2023-08-19', '2023-08-20', '2023-08-20']
-    })
-
-# Create the Orders dataframe
-orders = create_orders_dataframe()
-
-# Update the TotalPrice column in the orders dataframe to create updated_orders
-updated_orders = orders.copy()
-updated_orders['TotalPrice'] = updated_orders['TotalPrice'] * 100
-
-with st.expander("Data ⤵"):
-    st.write("Orders")
-    st.dataframe(orders)
-    st.write("Updated Orders")
-    st.dataframe(updated_orders)
-
-st.divider()
 st.write("CRUD Operations:")
 # Establishing a Google Sheets connection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Taking actions based on user input
 if st.button("New Entry"):
     new_entry = {}
     new_entry['OrderID'] = st.number_input("OrderID", min_value=0)
@@ -41,19 +15,14 @@ if st.button("New Entry"):
     new_entry['TotalPrice'] = st.number_input("TotalPrice", min_value=0)
     new_entry['OrderDate'] = st.date_input("OrderDate")
 
-    # Append new entry to orders dataframe
-    orders = orders.append(new_entry, ignore_index=True)
-    conn.create(worksheet="Orders", data=orders)
+    # Write new entry to Google Sheets
+    conn.write(data=[new_entry], worksheet="Orders")
     st.success("New Entry Added 🎉")
 
 if st.button("Calculate Total Orders Sum"):
     sql = 'SELECT SUM("TotalPrice") as "TotalOrdersPrice" FROM Orders;'
     total_orders = conn.query(sql=sql)  # default ttl=3600 seconds / 60 min
     st.dataframe(total_orders)
-
-if st.button("Update Worksheet"):
-    conn.update(worksheet="Orders", data=updated_orders)
-    st.success("Worksheet Updated 🤓")
 
 if st.button("Clear Worksheet"):
     conn.clear(worksheet="Orders")
