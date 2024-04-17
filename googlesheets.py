0a2dd9890c21885d0595f603cfd6f7d36f3441ee
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-04-17 13:15:23 +0100

    3 paginas

--- googlesheets.py
+++ googlesheets.py
@@ -13,13 +13,13 @@ st.title("🌟Loja da Quinta🌵")
 st.markdown("Sistema de controle de modelos.")
 
 # Sidebar navigation
-pagina_selecionada = st.sidebar.radio("Navegação", ["Vendas", "Reservas"])
+pagina_selecionada = st.sidebar.radio("Navegação", ["Stock","Reservation & Discount", "Existing Reservation"])
 
 # Estabelecendo uma conexão com o Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Página Vendas
-if pagina_selecionada == "Vendas":
+# Página Stock
+if pagina_selecionada == "Stock":
     # Fetch existing shoes data
     existing_data = conn.read(worksheet="Shoes", usecols=list(range(6)), ttl=5)
     existing_data = existing_data.dropna(how="all")
@@ -81,79 +81,95 @@ if pagina_selecionada == "Vendas":
         # Reload the page after updating the inventory
         st.experimental_rerun()
 
-# Página Reservas
-st.title("Reservation system")
-st.markdown("Type your data to be advised about new arrivals")
+# Página Stock
+if pagina_selecionada == "Reservation & Discount":
+    # Página Reservas
+    st.title("Reservation system")
+    st.markdown("Type your data to be advised about new arrivals")
 
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+    conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-existing_data = conn.read(worksheet="Reservations", usecols=list(range(6)), ttl=5)
-existing_data = existing_data.dropna(how="all")
+    existing_data = conn.read(worksheet="Reservations", usecols=list(range(6)), ttl=5)
+    existing_data = existing_data.dropna(how="all")
 
-# List of Business Types and Products
+    # List of Business Types and Products
 
-PRODUCTS = [
-    "Light Palha",
-    "Chuteirinha Vegana Preta",
-]
+    PRODUCTS = [
+        "Light Palha",
+        "Chuteirinha Vegana Preta",
+    ]
 
-with st.form(key="vendor_form"):
-    name = st.text_input(label="Name*")
-    email = st.text_input("e-mail")
-    whatsapp = st.text_input("whatsapp with international code")
-    products = st.multiselect("Wished shoes", options=PRODUCTS)
-    size = st.slider("Numeração", 34, 45, 34)
-    additional_info = st.text_area(label="Additional Notes")
+    with st.form(key="vendor_form"):
+        name = st.text_input(label="Name*")
+        email = st.text_input("e-mail")
+        whatsapp = st.text_input("whatsapp with international code")
+        products = st.multiselect("Wished shoes", options=PRODUCTS)
+        size = st.slider("Numeração", 34, 45, 34)
+        additional_info = st.text_area(label="Additional Notes")
 
-    # Mark mandatory fields
-    st.markdown("**required*")
+        # Mark mandatory fields
+        st.markdown("**required*")
 
-    submit_button = st.form_submit_button(label="Submit Details")
+        submit_button = st.form_submit_button(label="Submit Details")
 
-    # If the submit button is pressed
-    if submit_button:
-        # Check if all mandatory fields are filled
-        if not name:
-            st.warning("Ensure all mandatory fields are filled.")
-            st.stop()
-        elif existing_data["Name"].astype(str).str.contains(name).any():
-            st.warning("This name already exists.")
-            st.stop()
-        else:
-            # Create a new row of vendor data
-            vendor_data = pd.DataFrame(
-                [
-                    {
-                        "Name": name,
-                        "Email": email,
-                        "Whatsapp": whatsapp,
-                        "Products": ", ".join(products),
-                        "Size": size,
-                        "AdditionalInfo": additional_info,
-                    }
-                ]
-            )
+        # If the submit button is pressed
+        if submit_button:
+            # Check if all mandatory fields are filled
+            if not name:
+                st.warning("Ensure all mandatory fields are filled.")
+                st.stop()
+            elif existing_data["Name"].astype(str).str.contains(name).any():
+                st.warning("This name already exists.")
+                st.stop()
+            else:
+                # Create a new row of vendor data
+                vendor_data = pd.DataFrame(
+                    [
+                        {
+                            "Name": name,
+                            "Email": email,
+                            "Whatsapp": whatsapp,
+                            "Products": ", ".join(products),
+                            "Size": size,
+                            "AdditionalInfo": additional_info,
+                        }
+                    ]
+                )
 
-            # Add the new vendor data to the existing data
-            updated_df = pd.concat([existing_data, vendor_data], ignore_index=True)
+                # Add the new vendor data to the existing data
+                updated_df = pd.concat([existing_data, vendor_data], ignore_index=True)
 
-            # Update Google Sheets with the new vendor data
-            conn.update(worksheet="Reservations", data=updated_df)
+                # Update Google Sheets with the new vendor data
+                conn.update(worksheet="Reservations", data=updated_df)
 
-            st.success("Details successfully submitted!")
+                st.success("Details successfully submitted!")
 
-            # Clear the form fields after submission
-            name = ""
-            email = ""
-            whatsapp = ""
-            products = []
-            size = 34
-            additional_info = ""
+                # Clear the form fields after submission
+                name = ""
+                email = ""
+                whatsapp = ""
+                products = []
+                size = 34
+                additional_info = ""
 
-# Display existing reservation data
-st.subheader("Existing Reservations")
-if not existing_data.empty:
-    st.write(existing_data)
-else:
-    st.write("No existing reservations.")
+    # Display existing reservation data
+    st.subheader("Existing Reservations")
+    if not existing_data.empty:
+        st.write(existing_data)
+    else:
+        st.write("No existing reservations.")
 
+if pagina_selecionada == "Existing Reservations":
+    st.title("Active Reservations")
+
+    conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+
+    existing_data = conn.read(worksheet="Reservations", usecols=list(range(6)), ttl=5)
+    existing_data = existing_data.dropna(how="all")
+
+    # Display existing reservation data
+    st.subheader("Existing Reservations")
+    if not existing_data.empty:
+        st.write(existing_data)
+    else:
+        st.write("No existing reservations.")
