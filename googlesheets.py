8f1bb5abc724b2e4a91256753058c4ec70dc261e
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-05-02 11:59:49 +0100

    consulta

--- googlesheets.py
+++ googlesheets.py
@@ -75,7 +75,8 @@ if pagina_selecionada == "Verificação de estoque":
     # Filter the data based on the selected filters
     filtered_data = existing_data[
         (existing_data["Modelo"].isin(modelos_filtro)) & 
-        (existing_data["Número"].isin(numeros_filtro))
+        (existing_data["Número"].isin(numeros_europeus_selecionados))
+        
         
     ]
 
