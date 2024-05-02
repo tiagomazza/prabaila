ad591e851a4cc58626f1e73181d9752edc39b167
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-05-02 11:51:52 +0100

    numeros

--- googlesheets.py
+++ googlesheets.py
@@ -70,7 +70,7 @@ if pagina_selecionada == "Verificação de estoque":
     modelos = existing_data["Modelo"].unique()
     modelos_filtro = st.sidebar.multiselect("Filtrar por Modelo", modelos.astype(str), default=modelos.astype(str))
     
-    numeros_europeus_selecionados = st.multiselect("Quais números europeus deseja consultar?", numeros_disponiveis, default=[])
+    numeros_europeus_selecionados = st.multiselect("Quais números europeus deseja consultar?", numeros.astype(int), default=[])
 
     # Filter the data based on the selected filters
     filtered_data = existing_data[
