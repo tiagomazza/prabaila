dfb3221a09ed3b13a319317dabe5d42a7c011ebd
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-05-03 12:19:38 +0100

    filtro sem Nan

--- googlesheets.py
+++ googlesheets.py
@@ -58,8 +58,8 @@ pagina_selecionada = st.sidebar.radio("Página", ["Verificação de estoque","St
 
 if pagina_selecionada == "Verificação de estoque":
     # Fetch existing shoes data
-    existing_data = conn.read(worksheet="Shoes", usecols=["Modelo", "Número", "Imagem", "Descrição", "Preço", "Estoque", "Numero Brasileiro", "Deslize", "Amortecimento", "Cor da sola"], ttl=6), inplace=True)
-    existing_data = existing_data.dropna(how="all")
+    existing_data = conn.read(worksheet="Shoes", usecols=["Modelo", "Número", "Imagem", "Descrição", "Preço", "Estoque", "Numero Brasileiro", "Deslize", "Amortecimento", "Cor da sola"], ttl=6)
+    existing_data.dropna(subset=["Modelo", "Número", "Imagem", "Descrição", "Preço", "Estoque", "Numero Brasileiro", "Deslize", "Amortecimento", "Cor da sola"], inplace=True)
 
     # Sidebar filters
     st.sidebar.header("Filtros")
