df87423e3ec27287ea4a1f1edb9b2d90a5946351
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-05-20 11:19:46 +0100

    identado

--- googlesheets.py
+++ googlesheets.py
@@ -195,36 +195,36 @@ def register_page():
                movimentacao_type = ""
                additional_info = ""
 
-    st.title("Gerenciamento de Estoque WooCommerce")
-    st.title("Teste")
-    wcapi = API(
-    url="https://sua-loja.com",  # Substitua pelo URL da sua loja
-    consumer_key="ck_xxxxxxx",   # Substitua pela sua Consumer Key
-    consumer_secret="cs_xxxxxxx",  # Substitua pelo seu Consumer Secret
-    version="wc/v3"
-    )
-    # Formulário para entrada de dados
-    product_id = st.text_input("ID do Produto")
-    new_stock = st.number_input("Novo Estoque", min_value=0, step=1)
+        st.title("Gerenciamento de Estoque WooCommerce")
+        st.title("Teste")
+        wcapi = API(
+        url="https://sua-loja.com",  # Substitua pelo URL da sua loja
+        consumer_key="ck_xxxxxxx",   # Substitua pela sua Consumer Key
+        consumer_secret="cs_xxxxxxx",  # Substitua pelo seu Consumer Secret
+        version="wc/v3"
+        )
+        # Formulário para entrada de dados
+        product_id = st.text_input("ID do Produto")
+        new_stock = st.number_input("Novo Estoque", min_value=0, step=1)
 
-    if st.button("Atualizar Estoque"):
-        if product_id and new_stock is not None:
-            # Atualiza o estoque do produto no WooCommerce
-            data = {
-                "stock_quantity": new_stock
-            }
-            response = wcapi.put(f"products/{product_id}", data).json()
-            
-            if "id" in response:
-                st.success(f"Estoque do produto {product_id} atualizado para {new_stock}.")
+        if st.button("Atualizar Estoque"):
+            if product_id and new_stock is not None:
+                # Atualiza o estoque do produto no WooCommerce
+                data = {
+                    "stock_quantity": new_stock
+                }
+                response = wcapi.put(f"products/{product_id}", data).json()
+                
+                if "id" in response:
+                    st.success(f"Estoque do produto {product_id} atualizado para {new_stock}.")
+                else:
+                    st.error(f"Erro ao atualizar estoque: {response.get('message', 'Erro desconhecido')}")
             else:
-                st.error(f"Erro ao atualizar estoque: {response.get('message', 'Erro desconhecido')}")
-        else:
-            st.warning("Por favor, insira um ID de produto válido e quantidade de estoque.")
+                st.warning("Por favor, insira um ID de produto válido e quantidade de estoque.")
+            
+            
         
         
-    
-    
 
 
 # Imagem para exibir no menu lateral
