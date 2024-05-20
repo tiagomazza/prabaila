42a250051d62b5606d1e160f36820d9df18964d7
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-05-20 11:20:29 +0100

    retirado

--- googlesheets.py
+++ googlesheets.py
@@ -193,38 +193,7 @@ def register_page():
                value = 5
                movimentacao = 0
                movimentacao_type = ""
-               additional_info = ""
-
-        st.title("Gerenciamento de Estoque WooCommerce")
-        st.title("Teste")
-        wcapi = API(
-        url="https://sua-loja.com",  # Substitua pelo URL da sua loja
-        consumer_key="ck_xxxxxxx",   # Substitua pela sua Consumer Key
-        consumer_secret="cs_xxxxxxx",  # Substitua pelo seu Consumer Secret
-        version="wc/v3"
-        )
-        # Formulário para entrada de dados
-        product_id = st.text_input("ID do Produto")
-        new_stock = st.number_input("Novo Estoque", min_value=0, step=1)
-
-        if st.button("Atualizar Estoque"):
-            if product_id and new_stock is not None:
-                # Atualiza o estoque do produto no WooCommerce
-                data = {
-                    "stock_quantity": new_stock
-                }
-                response = wcapi.put(f"products/{product_id}", data).json()
-                
-                if "id" in response:
-                    st.success(f"Estoque do produto {product_id} atualizado para {new_stock}.")
-                else:
-                    st.error(f"Erro ao atualizar estoque: {response.get('message', 'Erro desconhecido')}")
-            else:
-                st.warning("Por favor, insira um ID de produto válido e quantidade de estoque.")
-            
-            
-        
-        
+               additional_info = ""        
 
 
 # Imagem para exibir no menu lateral
