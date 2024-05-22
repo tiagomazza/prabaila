d0d567da36281cb39a2834c473d03170c661e36d
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-05-22 23:19:05 +0100

    sync

--- googlesheets.py
+++ googlesheets.py
@@ -342,26 +342,31 @@ def teste_page():
         st.write("Esta é a página de teste. Aqui você pode adicionar funcionalidades de teste e desenvolvimento.")
         
         def sync_stock():
-            existing_data_shoes = load_existing_data("Shoes")
-            if existing_data_shoes is not None:
-                for index, row in existing_data_shoes.iterrows():
-                    id_ = row["ID"]
-                    stock = int(row["Estoque"])
-                    sales_quantity = get_sales_quantity(id_)
-                    new_stock = stock - sales_quantity
-                    data = {
-                        'stock_quantity': new_stock
-                    }
-                    try:
-                        wcapi.put(f"products/{id_}", data).json()
-                        st.success(f"Estoque atualizado para o produto ID {id_}: {new_stock}")
-                    except Exception as e:
-                        st.error(f"Erro ao atualizar o estoque para o produto ID {id_}: {e}")
-            else:
-                st.write("Nenhum dado encontrado na aba 'Shoes'.")
+        existing_data_shoes = load_existing_data("Shoes")
+        if existing_data_shoes is not None:
+            for index, row in existing_data_shoes.iterrows():
+                id_produto = row["ID_Produto"]
+                id_variacao = row["ID_Variação"]
+                stock = int(row["Estoque"])
+                sales_quantity = get_sales_quantity(id_variacao)  # Assumindo que a quantidade vendida está relacionada à variação
+                new_stock = stock - sales_quantity
+                data = {
+                    'stock_quantity': new_stock
+                }
+                try:
+                    if pd.isna(id_variacao) or id_variacao == "":
+                        # Atualizar o estoque do produto
+                        wcapi.put(f"products/{id_produto}", data).json()
+                        st.success(f"Estoque atualizado para o produto ID {id_produto}: {new_stock}")
+                    else:
+                        # Atualizar o estoque da variação do produto
+                        wcapi.put(f"products/{id_produto}/variations/{id_variacao}", data).json()
+                        st.success(f"Estoque atualizado para a variação ID {id_variacao} do produto ID {id_produto}: {new_stock}")
+                except Exception as e:
+                    st.error(f"Erro ao atualizar o estoque para o produto ID {id_produto} ou variação ID {id_variacao}: {e}")
+        else:
+            st.write("Nenhum dado encontrado na aba 'Shoes'.")
 
-        if st.button("Sync"):
-            sync_stock()
 
 if pagina_selecionada == "Active Reservations":
     active_reservations_page()
